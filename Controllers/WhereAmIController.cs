66f9a1e632486d4d8b5219b07af47ee99d812cc8
litchi <d0509327@mail.fcu.edu.tw>
2021-02-05 10:35:47 +0800

    Add Where am I controller

---
+++ Controllers/WhereAmIController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSharp_Traning.Controllers
+{
+    public class WhereAmIController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
