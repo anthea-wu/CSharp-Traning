93f241f0d618a7aef0048d8284139988e314efc5
litchi <d0509327@mail.fcu.edu.tw>
2021-02-05 10:42:45 +0800

    Get IP & Country code by api

--- Controllers/WhereAmIController.cs
+++ Controllers/WhereAmIController.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+
 namespace CSharp_Traning.Controllers
 {
+    [ApiController]
+    [Route("api/{controller}")]
     public class WhereAmIController : Controller
     {
+        
         // GET
         public IActionResult Index()
         {
-            return View();
+            return Ok(new {IP = "1.1.1.1", CountryCode = "TW"});
         }
     }
 }
